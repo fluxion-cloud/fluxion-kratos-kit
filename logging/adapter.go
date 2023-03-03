3a3b94deb7ea505ab727e68af0f5e502844b94a3
qiulin <liqiulin@outlook.com>
2023-03-04 00:04:57 +0800

    update: logging

--- logging/adapter.go
+++ logging/adapter.go
@@ -4,3 +4,7 @@ import "github.com/go-kratos/kratos/v2/log"
 
 type Adapter log.Logger
 type Logger log.Helper
+
+func NewLogger(adapter Adapter) *Logger {
+	return (*Logger)(log.NewHelper(adapter))
+}
